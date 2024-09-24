daa5b5776fd68124839d2cb33da4fe062731b1e0
JGilbert103 <145040882+JGilbert103@users.noreply.github.com>
2024-09-24 17:30:14 -0400

    Added editing of profiles
    Added the ability for a user to edit their profile with things such as username and email. Also changed the formatting for deleting messages and posts. Also changed formatting for user registration and login.

--- djangoTemp/base/views.py
+++ djangoTemp/base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 # Create your views here.
@@ -63,8 +63,9 @@ def registerPage(request):
             user.save()
             login(request, user)
             return(redirect('home'))
-    else:
-        messages.error(request, 'An error occured during registration')
+        else:
+            messages.error(request, 'An error occured during registration')
+            
     return render(request, 'base/login_register.html', {'form': form})
 
 def home(request):
@@ -170,3 +171,16 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj':message})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk = user.id)
+
+    return render(request, 'base/update-user.html', {'form': form})
